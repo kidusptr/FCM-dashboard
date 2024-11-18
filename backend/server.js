377364bcf14c0af249ace02127c42cadb9dbc4bd
Kidus Petros <kidusptr@gmail.com>
2024-11-18 10:41:15 +0300

    Cors implementation

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./configs/db_config.js";
 import { connectFCM } from "./configs/fcm_config.js";
 import userRoutes from "./routes/user.route.js";
@@ -8,7 +9,7 @@ import notificationRoutes from "./routes/notification.route.js";
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json()); // Parse JSON payloads
 
 app.get("/", (req, res) => res.send("Hello World!!!"));
@@ -17,16 +18,15 @@ app.get("/", (req, res) => res.send("Hello World!!!"));
 app.use("/api/users", userRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-// app.use( "/", (req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-
-// });
+app.use("/", (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.listen(3000, async () => {
   await connectDB();
